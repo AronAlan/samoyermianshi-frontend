60d27993833165bf48b82f39dacca6072664522d
Xuzhichao <x_zhichao@outlook.com>
2024-10-22 19:01:54 +0800

    修复因后端引入Sa-token造成的题目详情访问错误；修复反爬虫的计数重复问题

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true
-    }
+  output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true
+  },
+  reactStrictMode: false // 禁用 React 严格模式
 };
 
 export default nextConfig;
--- src/app/question/[questionId]/page.tsx
+++ src/app/question/[questionId]/page.tsx
@@ -1,34 +1,49 @@
-"use server";
+"use client";
 import { message } from "antd";
 import { getQuestionVoByIdUsingGet } from "@/api/questionController";
 import "./index.css";
+import { useEffect, useState } from "react";
 import QuestionCard from "@/compoents/QuestionCard";
 
 /**
  * 题目详情页
  * @constructor
  */
-export default async function QuestionPage({ params }: any) {
-    const { questionId } = params;
+export default function QuestionPage({ params }: any) {
+  const { questionId } = params;
+  //用于存储获取到的题目详情。
+  const [question, setQuestion]: any = useState(null);
+  //用于表示数据是否正在加载中。
+  const [loading, setLoading] = useState(true);
+  //用于存储可能发生的错误信息。
+  const [error, setError]: any = useState(null);
 
-    // 获取题目详情
-    let question = undefined;
-    try {
-        const res = (await getQuestionVoByIdUsingGet({
-            id: questionId
-        })) as any;
-        question = res.data;
-    } catch (e: any) {
+  useEffect(() => {
+    setLoading(true);
+    getQuestionVoByIdUsingGet({ id: questionId })
+      .then((res) => {
+        setQuestion(res.data);
+      })
+      .catch((e) => {
         message.error("获取题目详情失败，" + e.message);
-    }
-    // 错误处理
-    if (!question) {
-        return <div>获取题目详情失败，请刷新重试</div>;
-    }
+        setError("获取题目详情失败，请稍后重试。");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [questionId]); // 注意这里的依赖数组，确保只在 questionId 变化时重新请求
 
-    return (
-        <div id="questionPage">
-            <QuestionCard question={question} />
-        </div>
-    );
+  if (loading) {
+    return <div>加载中...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div id="questionPage">
+      <QuestionCard question={question} />
+    </div>
+  );
 }
